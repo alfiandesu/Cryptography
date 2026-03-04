385e1e90f23ef02341381ccacd1fac6564642ae7
Seydl1tz <143094146+alfiandesu@users.noreply.github.com>
2026-03-04 12:33:37 +0700

    Delete XOR/Known Plaintext directory

--- XOR/Known Plaintext/app.py
+++
@@ -1,9 +0,0 @@
-from pwn import xor
-
-ct = bytes.fromhex('') # changable depends on the chall 
-partial_key = xor(ct[:<plaintext length>], b"<plaintext>")  #changable depends on the chall
-
-for i in range(256):
-    key = partial_key + bytes([i])
-    decode = xor(ct, key)
-    print(decode)
