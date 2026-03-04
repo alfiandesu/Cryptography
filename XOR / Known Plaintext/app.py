1cf966c3e48280ff3866b0b47a0f8f688caa123c
Seydl1tz <143094146+alfiandesu@users.noreply.github.com>
2026-03-04 12:17:59 +0700

    Delete XOR / Known Plaintext directory

--- XOR / Known Plaintext/app.py
+++
@@ -1,7 +0,0 @@
-from pwn import xor
-
-flag = bytes.fromhex('') #changeable depends on the chall
-for i in range(128):
-    decode = (xor(flag, i))
-    if b'' in decode:  #changeable depends on the chall
-        print(f"{decode}")
