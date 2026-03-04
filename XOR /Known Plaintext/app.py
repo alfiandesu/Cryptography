314676a09938f246c8fa78b36be0d731ebe2bde4
Seydl1tz <143094146+alfiandesu@users.noreply.github.com>
2026-03-04 12:36:31 +0700

    Update app.py

--- XOR /Known Plaintext/app.py
+++ XOR /Known Plaintext/app.py
@@ -1,1 +1,9 @@
+from pwn import xor
 
+ct = bytes.fromhex('') # changeable depends on the chall
+partial_key = xor(ct[:<plaintext length>], b"<plaintext>") # changeable depends on the chall
+
+for i in range(256):
+    key = partial_key + bytes([i])
+    decode = xor(ct, key)
+    print(decode)
