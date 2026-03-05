c70ac619087c2fdc2616b9cc8366e43a5dad7497
Seydl1tz <143094146+alfiandesu@users.noreply.github.com>
2026-03-05 11:57:13 +0700

    Update app.py

--- XOR / Single Byte Key/app.py
+++ XOR / Single Byte Key/app.py
@@ -1,7 +1,7 @@
 from pwn import xor
 
-flag = bytes.fromhex('') # changable depends on the chall
+flag = bytes.fromhex('...')
 for i in range(128):
     decode = (xor(flag, i))
-    if b'' in decode: # changeable depends on the chall
+    if b'...' in decode:
         print(f"{decode}")
