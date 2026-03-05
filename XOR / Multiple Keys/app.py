6b3bf0e64766247994052343697111baf9b767d1
Seydl1tz <143094146+alfiandesu@users.noreply.github.com>
2026-03-05 11:56:43 +0700

    Set keys and ciphertext for XOR decryption
    Updated key and ciphertext values for XOR operation.

--- XOR / Multiple Keys/app.py
+++ XOR / Multiple Keys/app.py
@@ -1,7 +1,7 @@
 from pwn import xor
 
-k1 = bytes.fromhex('')   # changeable, depends on the chall
-k2_3 = bytes.fromhex('') # changeable, depends on the chall
-ct = bytes.fromhex('')   # changeable, depends on the chall
+k1 = bytes.fromhex('...')
+k2_3 = bytes.fromhex('...')
+ct = bytes.fromhex('...')
 
 print(xor(k1, k2_3, ct).decode)
