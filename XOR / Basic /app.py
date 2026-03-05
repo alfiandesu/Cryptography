7b82ce5c1e9b833fe107875972ba0533f002ef26
Seydl1tz <143094146+alfiandesu@users.noreply.github.com>
2026-03-05 11:56:14 +0700

    Update app.py

--- XOR / Basic /app.py
+++ XOR / Basic /app.py
@@ -1,6 +1,6 @@
 from pwn import xor
 
-ct = bytes.fromhex('') # changeable depends on the chall
-k = b'' # changeable depends on the chall
+ct = bytes.fromhex('...')
+k = b'...' 
 
-print(xor(ct, k).decode()) #changeable depends on the chall
+print(xor(ct, k).decode())
